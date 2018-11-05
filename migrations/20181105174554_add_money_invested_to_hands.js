f542212fc424c9823e4ad9f366f01e2866919f96
Adam <adt6261@gmail.com>
2018-11-05 18:40:28 -0500

    added money invested attribute to hands for determining up/down on sessions

--- migrations/20181105174554_add_money_invested_to_hands.js
+++ migrations/20181105174554_add_money_invested_to_hands.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return Promise.all([ 
     knex.schema.alterTable('hands', (tbl) => {
-      tbl.float('moneyInvested');
+      tbl.float('money_invested');
     })
   ]);
 };
