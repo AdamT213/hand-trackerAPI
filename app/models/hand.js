cbed68819ad4ee7c3da2990b3d573ab01e2f0baf
Adam <adt6261@gmail.com>
2019-02-20 16:09:55 -0500

    added cascade delete to hands and tables to allow true cascade deletion

--- app/models/hand.js
+++ app/models/hand.js
@@ -1,22 +1,27 @@
 "use strict";
 
-const bookshelf = require('../db/bookshelf');
+const bookshelf = require("../db/bookshelf");
+var cascadeDelete = require("bookshelf-cascade-delete"); 
+
+bookshelf.plugin(cascadeDelete);
 
 
 const Hand = bookshelf.Model.extend({
-  tableName: 'hands',
-  initialize: function() {
-  },
-  hasTimestamps: true,
-  sessions: function() {
-    return this.belongsTo('sessions');
-  },
-  tables: function() {
-    return this.belongsTo('tables');
-  },
-  hands_tags: function() {
-    return this.hasMany('hands_tags');
-  }
+	tableName: "hands",
+	initialize: function() {
+	},
+	hasTimestamps: true,
+	sessions: function() {
+		return this.belongsTo("sessions");
+	},
+	tables: function() {
+		return this.belongsTo("tables");
+	},
+	hands_tags: function() {
+		return this.hasMany("hands_tags");
+	},
+}, {
+	dependents: ["hands_tags"] 
 });
 
-module.exports = bookshelf.model('hands', Hand);
+module.exports = bookshelf.model("hands", Hand);
--- app/models/table.js
+++ app/models/table.js
@@ -1,22 +1,27 @@
 "use strict";
 
-const bookshelf = require('../db/bookshelf');
+const bookshelf = require("../db/bookshelf");
+var cascadeDelete = require("bookshelf-cascade-delete"); 
+
+bookshelf.plugin(cascadeDelete);
 
 
 const Table = bookshelf.Model.extend({
-  tableName: 'tables',
-  initialize: function() {
-  },
-  hasTimestamps: true,
-  hands: function() {
-    return this.hasMany('hands');
-  },
-  session: function() {
-    return this.belongsTo('sessions');
-  },
-  tables_tags: function() {
-    return this.hasMany('tables_tags');
-  }
+	tableName: "tables",
+	initialize: function() {
+	},
+	hasTimestamps: true,
+	hands: function() {
+		return this.hasMany("hands");
+	},
+	session: function() {
+		return this.belongsTo("sessions");
+	},
+	tables_tags: function() {
+		return this.hasMany("tables_tags");
+	}
+}, {
+	dependents: ["hands",  "tables_tags"] 
 });
 
-module.exports = bookshelf.model('tables', Table);
+module.exports = bookshelf.model("tables", Table);
