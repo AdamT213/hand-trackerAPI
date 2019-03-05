ed7fade40f21a2886f03b357364eff164d82a06d
Adam <adt6261@gmail.com>
2019-03-05 18:48:17 -0500

    might have to do this another way

--- app/visualizationRoutes/last30DaySessionData.js
+++ app/visualizationRoutes/last30DaySessionData.js
@@ -20,7 +20,8 @@ const Last30DaySessionData = async () => {
     const data = {};
     sessions.forEach(session => { 
         console.log(`made it this far`)
-        console.log(await (time(session.created_at)))
+        var created = await time(session.created_at)
+        console.log(created)
         console.log(monthAgo)
         if (session.isTermed && time(session.created_at) > monthAgo) { 
             console.log('still with ya')
