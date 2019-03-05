cdc32391ba4eb7748932aab21aaa22d3b8fcb88c
Adam <adt6261@gmail.com>
2019-03-05 16:55:00 -0500

    now were getting somewhere

--- app/visualizationRoutes/last30DaySessionData.js
+++ app/visualizationRoutes/last30DaySessionData.js
@@ -14,6 +14,7 @@ const totalAmount = session => {
 // fetch sessions from last 30 days
 const Last30DaySessionData = () => { 
     const sessions = Session.query('where', age('created_at', currentDay), '<', 2592000).fetch(); 
+    console.log(`sessions: ${sessions}`);
     // save data object with each date and total for the day
     const data = {};
     sessions.forEach(session => { 
