c68949dcd8622582d0eaf4b59793b1ad02d69993
Adam <adt6261@gmail.com>
2019-03-05 18:01:09 -0500

    now we should be getting somewhere

--- app/visualizationRoutes/last30DaySessionData.js
+++ app/visualizationRoutes/last30DaySessionData.js
@@ -4,6 +4,10 @@ const Session = require("../models/session");
 
 const monthAgo= new Date().getTime() - 2592000000;
 
+const time = timestamp => { 
+    new Date(timestamp).toString().replace(/-/g,"/").getTime()
+}
+
 const totalAmount = session => { 
     return session.status ? session.amount : -(session.amount);
 }
@@ -11,7 +15,7 @@ const totalAmount = session => {
 // fetch sessions from last 30 days
 const Last30DaySessionData = async () => { 
     try {
-    const sessions = await Session.query('where', await new Date('created_at').toString().replace(/-/g,"/").getTime(), '>', monthAgo).fetch(); 
+    const sessions = await Session.query('where', await time('created_at'), '>',  await monthAgo).fetch(); 
     console.log(`sessions: ${sessions}`);
     // save data object with each date and total for the day
     const data = {};
