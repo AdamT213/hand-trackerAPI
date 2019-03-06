32cb2d2a13930b58feb52bbf3da7ad729b0b51ae
Adam <adt6261@gmail.com>
2019-03-05 20:24:45 -0500

    refactored to return array of objects instead of object

--- app/visualizationRoutes/last30DaySessionData.js
+++ app/visualizationRoutes/last30DaySessionData.js
@@ -2,6 +2,8 @@ const Session = require("../models/session");
 
 // calculate how long ago session was started
 
+//TODO: try to refactor to filter out using time method in sql query
+
 const monthAgo= new Date().getTime() - 2592000000;
 
 const time = timestamp => { 
@@ -21,15 +23,16 @@ const Last30DaySessionData = async () => {
     try {
     const sessions = await Session.fetchAll(); 
     // save data object with each date and total for the day
-    const data = {};
+    const data = [];
     sessions.forEach(async session => {
         var created = await time(session.attributes.created_at);
         if (session.attributes.isTermed && created > monthAgo) { 
             const date = new Date(session.attributes.created_at)
-            console.log(`date: ${date}`);
-            if(data[date])
-                data[date] += totalAmount(session);
-            data[date] = totalAmount(session);
+            dataforDay = data.find(datum => datum.date === date);
+            if (dataforDay)
+                dataforDay.amount += totalAmount(session)
+            datum = {date: date,  amount: totalAmount(session)}
+            data.push(datum);
         }
     }); 
     return data;
